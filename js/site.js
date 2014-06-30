a218e5628834028103d004f81f65216a6b91e00b
Conor Wright <cwright@adventive.com>
2014-06-30 14:40:41 -0400

    made it so that work on the canvas is saved and reloaded next time you come

--- js/site.js
+++ js/site.js
@@ -86,6 +86,7 @@ function drop(event) {
 			$(this).addClass("elementSelected");
 			$("#widthMod").val($(this).css("width"));
 			$("#heightMod").val($(this).css("height"));
+			$("#depthMod").val($(this).css("z-index"));
 			$("#colorMod").val($(this).css("background-color"));
 		});
 	}
@@ -94,12 +95,14 @@ function drop(event) {
 	curDrag=undefined;
 	offset_data=undefined;
 	event.preventDefault();
+	log();
 	return false;
 }
 
 function setEditMenu(){
 	$("#widthMod").attr("oninput","fire('width', $('#widthMod').val())");
 	$("#heightMod").attr("oninput","fire('height', $('#heightMod').val())");
+	$("#depthMod").attr("oninput","fire('z-index', $('#depthMod').val())");
 	$("#colorMod").attr("oninput","fire('background-color', $('#colorMod').val())");
 	$("#rotateMod").attr("oninput","fire('rotate', $('#rotateMod').val())");
 }
@@ -111,5 +114,51 @@ function fire(css, val){
 	}
 	else{
 		$(".elementSelected").css(css, val);
+	}
+	log();
+}
+
+function mkLog(styl, clas, tg, source, html){
+	return {
+	style: styl, 
+	class: clas, 
+	tag: tg, 
+	src: source, 
+	inner: html
+	}
+}
+
+function log(){
+	var save = [];
+	$(".selectable").each(function(){
+		save[save.length] = mkLog(
+		$(this).attr("style"),
+		$(this).attr("class"),
+		$(this).get(0).tagName,
+		$(this).attr("src"),
+		$(this).get(0).innerHTML
+		);
+	});
+	window.localStorage.work = JSON.stringify(save);
+}
+
+function readIn(){
+	if(window.localStorage.work){
+		var save = JSON.parse(window.localStorage.work);
+		var cur;
+		for(var i=0; i<save.length; i++){
+			cur = save[i];
+			$("#canvas").append("<"+cur.tag+" class='"+cur.class+"' style='"+cur.style+"' src='"+cur.src+"' draggable='true' ondragstart='drag_start(event)'>"+cur.inner+"</"+cur.tag+">");
+		}
+		$(".selectable").each(function(index){
+			$(this).bind("mousedown", function(){
+				$(".elementSelected").removeClass("elementSelected");
+				$(this).addClass("elementSelected");
+				$("#widthMod").val($(this).css("width"));
+				$("#heightMod").val($(this).css("height"));
+				$("#depthMod").val($(this).css("z-index"));
+				$("#colorMod").val($(this).css("background-color"));
+			});
+		});
 	}
 }
